2c03cdb4faeba39b2f5bcd83ccdcba7d4fbdb1d6
Alberto <A01736390@tec.mx>
2023-10-24 14:21:24 -0600

    Buttons and Labels

--- Vaqueros/ViewController.swift
+++ Vaqueros/ViewController.swift
@@ -9,11 +9,38 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBAction func shoot(_ sender: UIButton) {
+    }
+    @IBAction func shield(_ sender: UIButton) {
+    }
+    @IBAction func reload(_ sender: UIButton) {
+    }
+    @IBAction func start(_ sender: UIButton) {
+    }
+    
+    @IBOutlet weak var life: UILabel!
+    @IBOutlet weak var ammo: UILabel!
+    @IBOutlet weak var enemylabel: UILabel!
+    @IBOutlet weak var enemylife: UILabel!
+    @IBOutlet weak var enemyammo: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func handleShoot(_ sender: UIButton) {
+    }
+    
+    @IBAction func handleShield(_ sender: UIButton) {
+    }
+    
+    @IBAction func handleReload(_ sender: UIButton) {
+    }
+    
+    @IBAction func startGame(_ sender: UIButton) {
+    }
+    
 }
 
