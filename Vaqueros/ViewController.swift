e68db3712a5a1f3253a234a0c63c5031d61883db
Alberto <A01736390@tec.mx>
2023-10-24 12:50:45 -0600

    Initial Commit

---
+++ Vaqueros/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Vaqueros
+//
+//  Created by Administrador on 24/10/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
