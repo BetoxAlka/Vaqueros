fb7960a0200f14e701ce16618b7dfa736f0654d1
Alberto <A01736390@tec.mx>
2023-10-24 14:37:10 -0600

    Corrections

--- Vaqueros/ViewController.swift
+++ Vaqueros/ViewController.swift
@@ -8,38 +8,35 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    @IBAction func shoot(_ sender: UIButton) {
-    }
-    @IBAction func shield(_ sender: UIButton) {
-    }
-    @IBAction func reload(_ sender: UIButton) {
-    }
-    @IBAction func start(_ sender: UIButton) {
-    }
+    
+    var userTurn = false
+    
+    @IBOutlet weak var shoot: UIButton!
+    @IBOutlet weak var shield: UIButton!
+    @IBOutlet weak var reload: UIButton!
+    @IBOutlet weak var start: UIButton!
     
     @IBOutlet weak var life: UILabel!
     @IBOutlet weak var ammo: UILabel!
-    @IBOutlet weak var enemylabel: UILabel!
     @IBOutlet weak var enemylife: UILabel!
     @IBOutlet weak var enemyammo: UILabel!
-    
+    @IBOutlet weak var enemylabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-    @IBAction func handleShoot(_ sender: UIButton) {
+    
+    @IBAction func handleshoot(_ sender: UIButton) {
     }
     
-    @IBAction func handleShield(_ sender: UIButton) {
+    @IBAction func handleshield(_ sender: UIButton) {
     }
     
-    @IBAction func handleReload(_ sender: UIButton) {
+    @IBAction func handlereload(_ sender: UIButton) {
     }
     
-    @IBAction func startGame(_ sender: UIButton) {
+    @IBAction func startGame(_ sender: Any) {
     }
     
 }
