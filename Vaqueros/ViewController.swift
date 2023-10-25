ef97d6e74fe056bf7827ab66f360a086f3e59c79
Alberto <A01736390@tec.mx>
2023-10-25 14:26:38 -0600

    First Conditions of Game

--- Vaqueros/ViewController.swift
+++ Vaqueros/ViewController.swift
@@ -35,6 +35,7 @@ class ViewController: UIViewController {
     @IBAction func p1Shoots(_ sender: UIButton) {
         if p1Turn {
             p1Attack = true
+            p1Turn = false
             p2Turn = true
         }
     }
@@ -42,6 +43,7 @@ class ViewController: UIViewController {
     @IBAction func p1Shields(_ sender: UIButton) {
         if p1Turn {
             p1Guard = true
+            p1Turn = false
             p2Turn = true
         }
     }
@@ -49,6 +51,7 @@ class ViewController: UIViewController {
     @IBAction func p1Reloads(_ sender: UIButton) {
         if p1Turn {
             p1Reload = true
+            p1Turn = false
             p2Turn = true
         }
     }
@@ -56,6 +59,7 @@ class ViewController: UIViewController {
     @IBAction func p2Shoots(_ sender: UIButton) {
         if p2Turn {
             p2Attack = true
+            p2Turn = false
             matchStart = true
             go.isHidden = false
         }
@@ -64,6 +68,7 @@ class ViewController: UIViewController {
     @IBAction func p2Shields(_ sender: UIButton) {
         if p2Turn {
             p2Guard = true
+            p2Turn = false
             matchStart = true
             go.isHidden = false
         }
@@ -72,6 +77,7 @@ class ViewController: UIViewController {
     @IBAction func p2Reloads(_ sender: UIButton) {
         if p2Turn {
             p2Reload = true
+            p2Turn = false
             matchStart = true
             go.isHidden = false
         }
@@ -94,7 +100,79 @@ class ViewController: UIViewController {
     }
     
     @IBAction func executeMatch(_ sender: UIButton) {
-        
+        if matchStart {
+            if p1Attack && p2Attack {
+                var p1Bullets = p1Ammo.text!
+                var p2Bullets = p2Ammo.text!
+                if p1Bullets != "0" && p2Bullets != "0" {
+                    var p1NewAmmo = Int(p1Bullets)!
+                    var p2NewAmmo = Int(p2Bullets)!
+                    p1NewAmmo = p1NewAmmo - 1
+                    p2NewAmmo = p2NewAmmo - 1
+                    p1Ammo.text = String(p1NewAmmo)
+                    p2Ammo.text = String(p2NewAmmo)
+                    
+                    var p1Lives = p1Life.text!
+                    var p1NewLife = Int(p1Lives)!
+                    var p2Lives = p2Life.text!
+                    var p2NewLife = Int(p2Lives)!
+                    p1NewLife = p1NewLife - 1
+                    p2NewLife = p2NewLife - 1
+                    p1Life.text = String(p1NewLife)
+                    p2Life.text = String(p2NewLife)
+                    
+                    matchStart = false
+                    p1Turn = true
+                }
+                else if p1Bullets != "0" && p2Bullets == "0" {
+                    var p1NewAmmo = Int(p1Bullets)!
+                    p1NewAmmo = p1NewAmmo - 1
+                    p1Ammo.text = String(p1NewAmmo)
+                    
+                    var p2Lives = p2Life.text!
+                    var p2NewLife = Int(p2Lives)!
+                    p2NewLife = p2NewLife - 1
+                    p2Life.text = String(p2NewLife)
+                    
+                    matchStart = false
+                    p1Turn = true
+                }
+                else if p1Bullets == "0" && p2Bullets != "0" {
+                    var p2NewAmmo = Int(p2Bullets)!
+                    p2NewAmmo = p2NewAmmo - 1
+                    p2Ammo.text = String(p2NewAmmo)
+                    
+                    var p1Lives = p1Life.text!
+                    var p1NewLife = Int(p1Lives)!
+                    p1NewLife = p1NewLife - 1
+                    p1Life.text = String(p1NewLife)
+                    
+                    matchStart = false
+                    p1Turn = true
+                }
+                else if p1Bullets == "0" && p2Bullets == "0" {
+                    matchStart = false
+                    p1Turn = true
+                }
+            }
+            else if p1Guard && p2Guard {
+                matchStart = false
+                p1Turn = true
+            }
+            else if p1Reload && p2Reload {
+                var p1Bullets = p1Ammo.text!
+                var p2Bullets = p2Ammo.text!
+                var p1NewAmmo = Int(p1Bullets)!
+                var p2NewAmmo = Int(p2Bullets)!
+                p1NewAmmo = p1NewAmmo + 1
+                p2NewAmmo = p2NewAmmo + 1
+                p1Ammo.text = String(p1NewAmmo)
+                p2Ammo.text = String(p2NewAmmo)
+                
+                matchStart = false
+                p1Turn = true
+            }
+        }
     }
     
     func showAlert(message: String) {
@@ -110,24 +188,18 @@ class ViewController: UIViewController {
         var p2Lives = p2Life.text
         if p1Lives == "0" {
             isGameOver = true
-            p1Turn = false
-            p2Turn = false
             start.isHidden = false
             go.isHidden = true
             showAlert(message: "P2 WINS")
         }
         else if p2Lives == "0" {
             isGameOver = true
-            p1Turn = false
-            p2Turn = false
             start.isHidden = false
             go.isHidden = true
             showAlert(message: "P1 WINS")
         }
         else if p1Lives == "0" && p2Lives == "0" {
             isGameOver = true
-            p1Turn = false
-            p2Turn = false
             start.isHidden = false
             go.isHidden = true
             showAlert(message: "It's a DRAW")
