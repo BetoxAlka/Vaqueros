302314b69fd2eb6c4e50cef995271e4055035014
Alberto <A01736390@tec.mx>
2023-10-25 13:58:40 -0600

    Player vs Player Interface and Code

--- Vaqueros/ViewController.swift
+++ Vaqueros/ViewController.swift
@@ -9,50 +9,92 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    var userTurn = false
-    var enemyGuard = false
-    var userGuard = false
+    var matchStart = false
+    var p1Turn = false
+    var p2Turn = false
+    var p1Attack = false
+    var p2Attack = false
+    var p1Guard = false
+    var p2Guard = false
+    var p1Reload = false
+    var p2Reload = false
     
-    @IBOutlet weak var shoot: UIButton!
-    @IBOutlet weak var shield: UIButton!
-    @IBOutlet weak var reload: UIButton!
     @IBOutlet weak var start: UIButton!
+    @IBOutlet weak var go: UIButton!
     
-    @IBOutlet weak var life: UILabel!
-    @IBOutlet weak var ammo: UILabel!
-    @IBOutlet weak var enemylife: UILabel!
-    @IBOutlet weak var enemyammo: UILabel!
-    @IBOutlet weak var enemylabel: UILabel!
+    @IBOutlet weak var p1Life: UILabel!
+    @IBOutlet weak var p1Ammo: UILabel!
+    @IBOutlet weak var p2Life: UILabel!
+    @IBOutlet weak var p2Ammo: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
     
-    @IBAction func handleshoot(_ sender: UIButton) {
-    }
-    
-    @IBAction func handleshield(_ sender: UIButton) {
-    }
-    
-    @IBAction func handlereload(_ sender: UIButton) {
-        if userTurn {
-            userGuard = false
-            let bullets = ammo.text!
-            let newammo = Int(bullets)
-            
+    @IBAction func p1Shoots(_ sender: UIButton) {
+        if p1Turn {
+            p1Attack = true
+            p2Turn = true
         }
     }
     
+    @IBAction func p1Shields(_ sender: UIButton) {
+        if p1Turn {
+            p1Guard = true
+            p2Turn = true
+        }
+    }
+    
+    @IBAction func p1Reloads(_ sender: UIButton) {
+        if p1Turn {
+            p1Reload = true
+            p2Turn = true
+        }
+    }
+    
+    @IBAction func p2Shoots(_ sender: UIButton) {
+        if p2Turn {
+            p2Attack = true
+            matchStart = true
+            go.isHidden = false
+        }
+    }
+    
+    @IBAction func p2Shields(_ sender: UIButton) {
+        if p2Turn {
+            p2Guard = true
+            matchStart = true
+            go.isHidden = false
+        }
+    }
+    
+    @IBAction func p2Reloads(_ sender: UIButton) {
+        if p2Turn {
+            p2Reload = true
+            matchStart = true
+            go.isHidden = false
+        }
+    }
+    
+    
+    
     @IBAction func startGame(_ sender: UIButton) {
-        enemyGuard = false
-        userGuard = false
-        enemylabel.text = "-"
-        enemylife.text = "3"
-        enemyammo.text = "0"
-        life.text = "3"
-        ammo.text = "0"
-        userTurn = true
+        p1Life.text = "3"
+        p1Ammo.text = "0"
+        p2Life.text = "3"
+        p2Ammo.text = "0"
+        start.isHidden = true
+        go.isHidden = false
+        p1Turn = true
+        p2Turn = false
+        p1Guard = false
+        p2Guard = false
+        matchStart = false
+    }
+    
+    @IBAction func executeMatch(_ sender: UIButton) {
+        
     }
     
     func showAlert(message: String) {
@@ -63,21 +105,37 @@ class ViewController: UIViewController {
     }
     
     func checkGameOver() -> Bool {
-        let lives = life.text
-        let enemy = enemylife.text
         var isGameOver = false
-        if lives == "0" {
+        var p1Lives = p1Life.text
+        var p2Lives = p2Life.text
+        if p1Lives == "0" {
             isGameOver = true
-            userTurn = false
-            showAlert(message: "Computer WINS")
+            p1Turn = false
+            p2Turn = false
+            start.isHidden = false
+            go.isHidden = true
+            showAlert(message: "P2 WINS")
         }
-        else if enemy == "0" {
+        else if p2Lives == "0" {
             isGameOver = true
-            userTurn = false
-            showAlert(message: "User WINS")
+            p1Turn = false
+            p2Turn = false
+            start.isHidden = false
+            go.isHidden = true
+            showAlert(message: "P1 WINS")
+        }
+        else if p1Lives == "0" && p2Lives == "0" {
+            isGameOver = true
+            p1Turn = false
+            p2Turn = false
+            start.isHidden = false
+            go.isHidden = true
+            showAlert(message: "It's a DRAW")
         }
         return isGameOver
     }
+    
+
     
 }
 
